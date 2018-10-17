9461b276dfd885cd68dbedabadefb909aa85e9f2
Yinka Ashafa <perpetualcoder@gmail.com>
2018-10-17 07:40:14 +0100

    Enable User Sign Up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
 
-  before_action :dont_allow_user_self_registration
-
-  private
-
-  def dont_allow_user_self_registration
-    if ['devise/registrations','devise_invitable/registrations'].include?(params[:controller]) && ['new','create'].include?(params[:action])
-      redirect_to root_path
-    end
-  end
+  # before_action :dont_allow_user_self_registration
+  #
+  # private
+  #
+  # def dont_allow_user_self_registration
+  #   if ['devise/registrations','devise_invitable/registrations'].include?(params[:controller]) && ['new','create'].include?(params[:action])
+  #     redirect_to root_path
+  #   end
+  # end
 
 end
