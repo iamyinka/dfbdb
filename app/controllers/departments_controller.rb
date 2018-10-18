4b1c991d1ae445731053166fd5b5045d6cee5702
Yinka Ashafa <perpetualcoder@gmail.com>
2018-10-18 02:03:44 +0100

    Create Departments Index Controller & View

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,3 +1,18 @@
 class DepartmentsController < ApplicationController
   #code
+
+  def index
+    @departments = Department.all
+  end
+
+
+  private
+
+  def set_dept
+    @department = Department.find(params[:id])
+  end
+
+  def dept_params
+    params.require(:department).permit(:dept_name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :departments
   resources :staffs
   devise_for :users
   root "staffs#index"
